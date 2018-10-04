fa406a426b43b54a8bab2f3b344abafc4014dd6b
Ayush Shivani <ayushshivani12345@gmail.com>
2018-10-04 22:24:25 +0530

    commnets

--- game.cpp
+++ game.cpp
@@ -28,7 +28,6 @@ void make_board(void)
 
 int check_com(int x)
 {
-	// checking horizontally
 	for (int i = 0; i < 3; ++i)
 	{
 		if (board[i][0]==board[i][1] && board[i][1]==board[i][2] && board[i][0]!='_')
